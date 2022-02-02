d8185a806c55bbf959a038911517ee65c8d23a06
村上恭哉 <mkkyyyy@mkkyyyy.local>
2022-02-02 19:22:14 +0900

    Send message to slack

--- src/bot/Slackbot.py
+++ src/bot/Slackbot.py
@@ -1,6 +1,14 @@
+import requests
+# .env ファイルをロードして環境変数へ反映
+from dotenv import load_dotenv
+load_dotenv()
 
+# 環境変数を参照
+import os
 
 class Slackbot():
+    TOKEN = os.getenv("TOKEN")
+    CHANNEL = os.getenv("CHANNEL")
 
     def get_message():
         # please complete
@@ -11,6 +19,14 @@ class Slackbot():
 
         return "slander"
 
-    def send_slander_message_to_slack():
+    def send_slander_message_to_slack(self, message):
+        url = "https://slack.com/api/chat.postMessage"
+        headers = {"Authorization": "Bearer "+ self.TOKEN}
+        data  = {
+            'channel': self.CHANNEL,
+            'text': 'テストです。'
+        }
+        r = requests.post(url, headers=headers, data=data)
+        print("return ", r.json())
 
         return "slander"
--- src/bot/main.py
+++ src/bot/main.py
@@ -1,7 +1,12 @@
-
+from Slackbot import Slackbot
 
 def main():
-    print("aaaa")
+    bot = Slackbot()
+    
+
+
+    bot.send_slander_message_to_slack("test")
+
 
 
 if __name__ == "__main__":
