46e586f9a44399fdb7c156e10f128dcce7f11b1d
村上恭哉 <mkkyyyy@mkkyyyy.local>
2022-02-04 14:45:39 +0900

    terraform init

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-northeast-1"
+}
