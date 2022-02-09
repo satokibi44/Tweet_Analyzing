3390508aaac5939e38564faa07bdb356d4419f0d
Fan Youmei <youmeifan99@gmail.com>
2022-02-09 23:17:35 +0800

    get the message and send to api

--- src/bot/Slackbot.py
+++ src/bot/Slackbot.py
@@ -15,24 +15,27 @@ class Slackbot():
         url = "https://slack.com/api/conversations.history"
         token = "aaaaaa"
         header = {
-            "Authorization":"Bearer {}".format(token)
+            "Authorization": "Bearer {}".format(token)
         }
         payload = {
-            "channel":"AAAAAAAA"
-            #"channel": self.CHANNEL
+            "channel": "AAAAAAAA"
+            # "channel": self.CHANNEL
         }
         message = requests.get(url, headers=header, params=payload)
         return "message"
 
-
-    def send_message_to_slander_api():
-
-        return "slander"
+    def send_message_to_slander_api(self,message):
+        url = 'http://3.143.237.69/kusorep/score/'
+        message = message
+        param = {'msg': message}
+        res = requests.get(url, params=param)
+        slander = res.json()
+        return slander
 
     def send_slander_message_to_slack(self, message):
         url = "https://slack.com/api/chat.postMessage"
-        headers = {"Authorization": "Bearer "+ self.TOKEN}
-        data  = {
+        headers = {"Authorization": "Bearer " + self.TOKEN}
+        data = {
             'channel': self.CHANNEL,
             'text': 'テストです。'
         }
