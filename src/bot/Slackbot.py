a50866769561bf14bc9257e737447a7d2db5c8d9
村上恭哉 <mkkyyyy@mkkyyyy.local>
2022-02-10 16:44:24 +0900

    Conbine all function

--- src/bot/Slackbot.py
+++ src/bot/Slackbot.py
@@ -20,7 +20,7 @@ class Slackbot():
             "Authorization": "Bearer {}".format(token)
         }
         payload = {
-            "channel": self.CHANNEL
+            "channel": "C030LTJ6QAX"
         }
         message = requests.get(url, headers=header, params=payload)
         content_bin = message.content.decode("utf-8")
@@ -38,17 +38,14 @@ class Slackbot():
         return slander
 
     # Get measurement result from slander level api and check whether it is a slander
-    def get_measurement_result(self):
+    def get_measurement_result(self, measurement_result_from_slander_api):
 
         # get slander level from slander level api
-        message = self.get_message()
-        measurement_result_from_slander_api = self.send_message_to_slander_api(message)
         measurement_result = measurement_result_from_slander_api[0]
-        # print(measurement_result)
 
         # Check wether the message is a slander (> 0.6 is a slander)
         if measurement_result > 0.6:
-            print("This is a Slander::")
+            print("This is a Slander")
             return "This is a Slander"
         else:
             print(None)
@@ -60,11 +57,16 @@ class Slackbot():
         headers = {"Authorization": "Bearer " + self.TOKEN}
         data = {
             'channel': self.CHANNEL,
-            'text': message + slander,
+            'text': slander + " `" + message + "`",
         }
+        if slander is None:
+            print("No slander")
+            return False
         for i in range(5):
             r = requests.post(url, headers=headers, data=data)
             if r.json()["ok"]:
                 return True
 
+        print("Error")
+        print(r.json())
         return False
--- src/bot/main.py
+++ src/bot/main.py
@@ -2,7 +2,10 @@ from Slackbot import Slackbot
 
 def main():
     bot = Slackbot()
-    
+    message = bot.get_message()
+    result_from_slander_api = bot.send_message_to_slander_api(message)
+    slander = bot.get_measurement_result(result_from_slander_api)
+    bot.send_slander_message_to_slack(message, slander)
 
 if __name__ == "__main__":
     main()
